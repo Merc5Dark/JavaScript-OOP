5db1afb6ca51b9c6bb66caf518e51596763af64c
Kenneth Siyabonga Ncube <140717072+Merc5Dark@users.noreply.github.com>
2023-07-29 15:46:39 +0200

    Add files via upload

---
+++ 06-ES6-Tooling/02-es6-tooling.js
@@ -0,0 +1,4 @@
+
+// Transpiler (Transpile and compile js into all browser compatible code) -> BABEL
+// Bundler (Combiles all JS files into a bundle) -> WEBPACK
+
---
+++ 06-ES6-Tooling/es6-tooling/babel-demo/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let _circle = require("./circle.js");
+
+let c = new _circle.Circle(11);
+c.draw();
