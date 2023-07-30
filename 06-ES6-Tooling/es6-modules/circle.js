a7f486bc219b5daf719e113e2abe252b89df41e0
Kenneth Siyabonga Ncube <140717072+Merc5Dark@users.noreply.github.com>
2023-07-30 11:11:18 +0200

    Update circle.js

--- 06-ES6-Tooling/es6-modules/circle.js
+++ 06-ES6-Tooling/es6-modules/circle.js
@@ -1,12 +1,17 @@
-
+// Using the WeakMap data structure to store the private property '_radius'
 const _radius = new WeakMap();
 
+// Defining the Circle class
 export class Circle {
+  // Constructor for creating Circle objects with a given radius
   constructor(radius) {
+    // Using WeakMap to associate the private '_radius' property with each Circle instance
     _radius.set(this, radius);
   }
 
+  // Method for drawing the Circle and displaying its radius
   draw() {
+    // Retrieving the radius from the WeakMap associated with the current instance
     console.log('Circle with radius ' + _radius.get(this));
   }
 }
