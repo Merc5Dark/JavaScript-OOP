d2c0df5dfa5583a76737141ef229f574f2b6edb3
Kenneth Siyabonga Ncube <140717072+Merc5Dark@users.noreply.github.com>
2023-07-30 11:11:57 +0200

    Update index.js

--- 06-ES6-Tooling/es6-modules/index.js
+++ 06-ES6-Tooling/es6-modules/index.js
@@ -1,5 +1,11 @@
+// Importing the Circle class from the 'circle.js' module
+import { Circle } from './circle.js';
 
-import {Circle} from './circle.js';
-
+// Creating a new Circle instance with radius 11
 const c = new Circle(11);
+
+// Calling the 'draw' method on the Circle instance to display the circle's information
 c.draw();
+
+// Output:
+// Circle with radius 11
